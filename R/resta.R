9247083bd449d11a8e54e6de325533b7e507a0fd
Francisco Varela <franxvarela@yahoo.es>
2020-12-29 09:34:24 +0100

    Commit inicial

---
+++ R/resta.R
@@ -0,0 +1,17 @@
+#' Resta de dos números
+#'
+#' Función que realiza la resta entre dos números
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La resta entre a y b
+#' @export
+#'
+#' @seealso \link{suma}
+#'
+#' @examples
+#' resta(5,4)
+resta <- function(a,b){
+  return(a-b)
+}
---
+++ R/suma.R
@@ -0,0 +1,17 @@
+#' Suma de dos números
+#'
+#' Función que realiza la suma entre dos números
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La suma entre a y b
+#' @export
+#'
+#' @seealso \link{resta}
+#'
+#' @examples
+#' suma(5,4)
+suma <- function(a,b){
+  return(a+b)
+}
