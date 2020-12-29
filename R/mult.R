bd690f3844410e79faadd2e7715eac3716e93720
Francisco Varela <franxvarela@yahoo.es>
2020-12-29 12:21:18 +0100

    Se han añadido las funciones mult y div

---
+++ R/mult.R
@@ -0,0 +1,18 @@
+#' Multiplicación de dos números
+#'
+#' Función que realiza la multiplicación entre dos números
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La multiplicación entre a y b
+#' @export
+#'
+#' @seealso \link{div}
+#'
+#' @examples
+#' mult(5,4)
+
+mult <- function(a,b) {
+  return(a*b)
+}
